f7c5c6c5dd0913f5644ed1321183690b7ea3ed0b
ramirotem01 <132000105+ramirotem01@users.noreply.github.com>
2026-01-30 14:51:49 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,19 @@
 // firebase.js
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyAwduvJv0z1T5dyV724zmkO83hj9SJFKf4",
+  authDomain: "habittrackermultiuser.firebaseapp.com",
+  projectId: "habittrackermultiuser",
+  storageBucket: "habittrackermultiuser.appspot.com",
+  messagingSenderId: "59091703051",
+  appId: "1:59091703051:web:ef768b4823f3c77e58fe0e",
+  measurementId: "G-HWNWP0C8DE"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+// Initialize services
 const auth = firebase.auth();
 const db = firebase.firestore();
