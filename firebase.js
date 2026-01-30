a6284171313fe241c8bddcdb982a212a79fbcceb
ramirotem01 <132000105+ramirotem01@users.noreply.github.com>
2026-01-30 14:34:36 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,14 @@
-// הנתונים שלך מ-Firebase Console
+// Firebase App
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
   messagingSenderId: "SENDER_ID",
   appId: "APP_ID"
 };
 
-// אתחול
 firebase.initializeApp(firebaseConfig);
 
-// קיצורי דרך
 const auth = firebase.auth();
 const db = firebase.firestore();
