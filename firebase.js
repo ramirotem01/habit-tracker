bf3e742a48610075aad1a1e939847860f15e512c
ramirotem01 <132000105+ramirotem01@users.noreply.github.com>
2026-01-30 14:38:21 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,12 @@
-// Firebase App
+// firebase.js
+
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
 };
 
 firebase.initializeApp(firebaseConfig);
