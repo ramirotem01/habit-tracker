19732c0dde8fc28c3efaa310a0285c7717291204
ramirotem01 <132000105+ramirotem01@users.noreply.github.com>
2026-01-31 10:29:41 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'habits-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/dashboard.html',
+  '/style.css',
+  '/dashboard.js',
+  '/firebase.js'
+];
+
+// התקנה ושימור קבצים בזיכרון
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// הפעלה מתוך הזיכרון לביצועים מהירים
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
